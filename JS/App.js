e24d785b0827c9beb039ebffd5a80cc97df8e70f
ImranHossain5722 <imd572258@gmail.com>
2022-06-28 13:50:24 +0600

    build bule box movement

---
+++ JS/App.js
@@ -0,0 +1,33 @@
+setInterval(changeToRelativeAxis, 1000);
+
+function changeToRelativeAxis() {
+  var element = document.getElementById('boxId'),
+      rect = element.getBoundingClientRect(),
+      move = 50; // in pixel
+  
+  var goRight = ( (rect.right+move) <= window.innerWidth) ? true : false;
+  var goBottom = ( (rect.bottom+move) <= window.innerHeight ) ? true : false;
+  
+  if( goRight && goBottom) 
+  {
+    element.style.top = (rect.top+move) + "px";
+    element.style.left = (rect.left+move) + "px";
+  } 
+  else if( !goRight && goBottom ) 
+  {
+    element.style.top = (rect.top+move) + "px";
+    element.style.left = (rect.left-move) + "px";
+  } 
+  else if( goRight && !goBottom ) {
+    element.style.top = (rect.top-move) + "px";
+    element.style.left = (rect.left+move) + "px";
+  }
+  else
+  {
+    element.style.top = (rect.top-move) + "px";
+    element.style.left = (rect.left-move) + "px";
+  }
+  
+
+}
+
